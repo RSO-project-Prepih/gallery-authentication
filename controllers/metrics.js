c58409c6c746fbcf2fdd9850f3f6333c24928afa
Tine Crnugelj <tine.crnugelj@gmail.com>
2024-01-09 16:37:53 +0100

    metrics third fix

--- controllers/metrics.js
+++ controllers/metrics.js
@@ -4,7 +4,7 @@ const { connectionAttempts } = require('../models/db');
 const metrics = async (req, res) => {
     const numberOfUsers = (await User.findAll()).length;
     return res.status(200).type('text/plain').send(
-        'Connection attempts: ' + connectionAttempts + '\nNumber of users: ' + numberOfUsers
+        'Connection attempts: ' + connectionAttempts.toString() + '\nNumber of users: ' + numberOfUsers.toString()
     );
 };
 
