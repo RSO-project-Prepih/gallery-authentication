ed42c26db5fd5d011a3c04f55abdaad7cd0fd15a
Tine Crnugelj <tine.crnugelj@gmail.com>
2024-01-09 16:34:51 +0100

    Metrics second fix, max connections 1 for db

--- controllers/metrics.js
+++ controllers/metrics.js
@@ -3,10 +3,8 @@ const { connectionAttempts } = require('../models/db');
 
 const metrics = async (req, res) => {
     const numberOfUsers = (await User.findAll()).length;
-    return res.status(200).send(
-        `<p>Connection attempts: ${connectionAttempts} 
-        <br/>
-        Number of users: ${numberOfUsers}</p>`
+    return res.status(200).type('text/plain').send(
+        'Connection attempts: ' + connectionAttempts + '\nNumber of users: ' + numberOfUsers
     );
 };
 
