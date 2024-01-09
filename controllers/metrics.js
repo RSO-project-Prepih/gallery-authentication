559265a2d75cd473a19a5ebd0f3fe76b673c131c
Tine Crnugelj <tine.crnugelj@gmail.com>
2024-01-09 16:25:22 +0100

    Display metrics as string

--- controllers/metrics.js
+++ controllers/metrics.js
@@ -3,10 +3,11 @@ const { connectionAttempts } = require('../models/db');
 
 const metrics = async (req, res) => {
     const numberOfUsers = (await User.findAll()).length;
-    return res.status(200).json({
-        'Connection attempts': connectionAttempts,
-        'Number of users': numberOfUsers,
-    });
+    return res.status(200).send(
+        `<p>Connection attempts: ${connectionAttempts} 
+        <br/>
+        Number of users: ${numberOfUsers}</p>`
+    );
 };
 
 module.exports = {
